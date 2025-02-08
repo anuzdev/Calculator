13be0974cb7f3eee57889f6365721a4c6d4ef79e
Anuj Khatri <167432970+anuzdev@users.noreply.github.com>
2025-02-08 13:13:47 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const result = document.getElementById('result');
+function appendValue(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+}
+
+function deleteLast() {
+  result.value = result.value.slice(0, -1);
+
+}
+
+function percent() {
+  result.value = result.value / 100;
+}
+
+function calculateResult() {
+  try {
+    result.value = eval(result.value);
+  }
+  catch (error) {
+    result.value = "Error";
+  }
+}
